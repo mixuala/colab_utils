00dfd38934f4e6daa3c67e57de8166700ea14437
michael lin <mlininkl@gmail.com>
2018-02-05 14:51:32 +0800

    weird format error?

--- gcloud.py
+++ gcloud.py
@@ -16,47 +16,46 @@
 
 
 
-Note: these methods currently use ipython magic commands and therefore cannot be loaded 
-  from a module at this time. For now, you can copy/paste the entire script to a 
-  colaboratory cell to run.
+  Note: these methods currently use ipython magic commands and therefore cannot be loaded 
+    from a module at this time. For now, you can copy/paste the entire script to a 
+    colaboratory cell to run.
 
 
 
-Long-running training sessions on `colaboratory` VMs are at risk of reset after 90 mins of
-inactivity or shutdown after 12hrs of training. This script allows you to save/restore
-checkpoints to Google Cloud Storage to avoid losing your results.
+  Long-running training sessions on `colaboratory` VMs are at risk of reset after 90 mins of
+  inactivity or shutdown after 12hrs of training. This script allows you to save/restore
+  checkpoints to Google Cloud Storage to avoid losing your results.
 
 
-************************************
-* A simple working script *
-************************************
-```
-import os
-import colab_utils.gcloud
+  ************************************
+  * A simple working script *
+  ************************************
+  ```
+  import os
+  import colab_utils.gcloud
 
-# authorize access to Google Cloud SDK from `colaboratory` VM
-project_name = "my-project-123"
-gcloud.gcloud_auth(project_name)
+  # authorize access to Google Cloud SDK from `colaboratory` VM
+  project_name = "my-project-123"
+  colab_utils.gcloud.gcloud_auth(project_name)
 
-# set paths
-ROOT = %pwd
-LOG_DIR = os.path.join(ROOT, 'log')
-TRAIN_LOG = os.path.join(LOG_DIR, 'training-run-1')
+  # set paths
+  ROOT = %pwd
+  LOG_DIR = os.path.join(ROOT, 'log')
+  TRAIN_LOG = os.path.join(LOG_DIR, 'training-run-1')
 
-# save latest checkpoint as a zipfile to a GCS bucket `gs://my-checkpoints/`
-#     zipfile name = "{}.{}.zip".format() os.path.basename(TRAIN_LOG), global_step)
-#                     e.g. gs://my-checkpoints/training-run-1.1000.zip"
-bucket_name = "my-checkpoints"
-gcloud.save_to_bucket(TRAIN_LOG, bucket_name, save_events=True, force=False)
+  # save latest checkpoint as a zipfile to a GCS bucket `gs://my-checkpoints/`
+  #     zipfile name = "{}.{}.zip".format() os.path.basename(TRAIN_LOG), global_step)
+  #                     e.g. gs://my-checkpoints/training-run-1.1000.zip"
+  bucket_name = "my-checkpoints"
+  colab_utils.gcloud.save_to_bucket(TRAIN_LOG, bucket_name, save_events=True, force=False)
 
 
-# restore a zipfile from GCS bucket to a local directory, usually in  
-#     tensorboard `log_dir`
-CHECKPOINTS = os.path.join(LOG_DIR, 'training-run-2')
-zipfile = os.path.basename(TRAIN_LOG)   # training-run-1
-gcloud.load_from_bucket("training-run-1.1000.zip", bucket_name, CHECKPOINTS )
-
-```
+  # restore a zipfile from GCS bucket to a local directory, usually in  
+  #     tensorboard `log_dir`
+  CHECKPOINTS = os.path.join(LOG_DIR, 'training-run-2')
+  zipfile = os.path.basename(TRAIN_LOG)   # training-run-1
+  colab_utils.gcloud.load_from_bucket("training-run-1.1000.zip", bucket_name, CHECKPOINTS )
+  ```
 
 """
 
@@ -64,12 +63,11 @@ import os
 import shutil
 import subprocess
 import tensorflow as tf
-from google.colab import auth
 
 __all__ = [
   'gcloud_auth', 
   'save_to_bucket',
-  'load_from_bucket',
+  'load_from_bucket'
 ]
 
 def _shell(cmd):
@@ -93,6 +91,7 @@ def gcloud_auth(project_id):
   Return:
     GCS project id
   """
+  from google.colab import auth
   # authenticate user and set project
   auth.authenticate_user()
   # project_id = "my-project-123"
